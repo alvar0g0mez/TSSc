ae2b2afe1b01032e99422bb03ce4af65e0cd1f69
alvaro <alvaro.gom.000@gmail.com>
2025-05-11 17:34:40 +0200

    Add to exploration of fluorescence data

--- R/fluorescence_data_processing.R
+++ R/fluorescence_data_processing.R
@@ -4,10 +4,11 @@ library(xlsx)
 library(data.table)
 library(dplyr)
 library(tidyr)
+library(readxl)
 
 
 # Set up environment
-working_from = "charite"
+working_from = "home"
 
 if (working_from == "home") {
   base_dir = "/home/alvaro/MyStuff/TSSc/"
@@ -19,14 +20,16 @@ if (working_from == "home") {
 
 
 # Load original data
-raw_1 <- as.data.frame(fread(paste(base_dir, "Data/Boone_lab/Fluorescence/raw_1.csv", sep = "")))
-raw_2 <- as.data.frame(fread(paste(base_dir, "Data/Boone_lab/Fluorescence/raw_2.csv", sep = "")))
-raw_3 <- as.data.frame(fread(paste(base_dir, "Data/Boone_lab/Fluorescence/raw_3.csv", sep = "")))
+raw_1 <- read_xlsx(paste(base_dir, "Data/Boone_lab/Fluorescence/het_diploid_intensities_forMC.xlsx", sep=""), sheet = 2)
+raw_2 <- read_xlsx(paste(base_dir, "Data/Boone_lab/Fluorescence/het_diploid_intensities_forMC.xlsx", sep=""), sheet = 3)
+raw_3 <- read_xlsx(paste(base_dir, "Data/Boone_lab/Fluorescence/het_diploid_intensities_forMC.xlsx", sep=""), sheet = 4)
 
 
+# Save it as .csv
+fwrite(raw_1, paste(base_dir, "Data/Boone_lab/Fluorescence/raw_1.csv", sep = ""))
+fwrite(raw_2, paste(base_dir, "Data/Boone_lab/Fluorescence/raw_2.csv", sep = ""))
+fwrite(raw_3, paste(base_dir, "Data/Boone_lab/Fluorescence/raw_3.csv", sep = ""))
 
-# Put together replicates
-full_fluorescence <- rbind(rbind(raw_1, raw_2), raw_3)
 
 
 
